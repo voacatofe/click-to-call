e7ee1be1969ff0e88e156110ccf87616c584197d
Developer <user@example.com>
2025-07-04 20:49:33 -0300

    ajustes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,13 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
